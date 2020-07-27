672fcac226699167a2937905f21e8835e00edaeb
Joseph Croot <josephcroot@hotmail.com>
2020-07-27 11:32:12 +0100

    Setting gameweek to 47 as season has ended

--- src/main/java/com/josephcroot/fantasyfootballAPI/GameweekData.java
+++ src/main/java/com/josephcroot/fantasyfootballAPI/GameweekData.java
@@ -13,26 +13,27 @@ public class GameweekData {
 	
 	/* Set gameweek from API */
 	public static void setGameweek() throws JSONException, IOException {
-		JSONArray gameweekInfo = FantasyFootballAPI.getJSONArrayFromJSONObject("bootstrap-static/",null,"events");
-		for (int i = 0; i < gameweekInfo.length(); i++) {
-			JSONObject current = gameweekInfo.getJSONObject(i);
-			if (current.getBoolean("is_next") == true) {
-				gameweek = current.getInt("id") - 1;
-			}
-		}
+		//JSONArray gameweekInfo = FantasyFootballAPI.getJSONArrayFromJSONObject("bootstrap-static/",null,"events");
+		//for (int i = 0; i < gameweekInfo.length(); i++) {
+		//	JSONObject current = gameweekInfo.getJSONObject(i);
+		//	if (current.getBoolean("is_next") == true) {
+		//		gameweek = current.getInt("id") - 1;
+		//	}
+		//}
 	}
 	
 	/* Get gameweek method */
 	public static int getGameweek() throws JSONException, IOException {
-		if (gameweek == 0)
-			setGameweek();
-		return gameweek;
+		//if (gameweek == 0)
+		//	setGameweek();
+		//return gameweek;
+		return 47;
 	}
 	
 	/* Schedule to poll API and update the gameweek */
-	@Scheduled(fixedDelay = 100000)
-	public void updateGameweek() throws JSONException, IOException {
-		setGameweek();
-	}
+	//@Scheduled(fixedDelay = 100000)
+	//public void updateGameweek() throws JSONException, IOException {
+	//	setGameweek();
+	//}
 
 }
